c92448477e461b06f0d514dac9690d495e2871f5
LhenardT <lr.ltrinidad@mmdc.mcl.edu.ph>
2025-10-08 06:15:53 +0800

    Delete petTapp-be directory

--- petTapp-be/src/services/file/index.ts
+++
@@ -1,6 +0,0 @@
-export { IFileService } from './IFileService';
-export { FileService } from './FileService';
-
-// Export singleton instance
-import { FileService } from './FileService';
-export const fileService = new FileService();
--- petTapp-be/src/services/storage/index.ts
+++
@@ -1,6 +0,0 @@
-export { IStorageService } from './IStorageService';
-export { SupabaseStorageService } from './SupabaseStorageService';
-
-// Export singleton instance
-import { SupabaseStorageService } from './SupabaseStorageService';
-export const storageService = new SupabaseStorageService();
