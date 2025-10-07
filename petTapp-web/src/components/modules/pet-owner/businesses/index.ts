6fde9a406ab47e623f8364c890331faa1a8dd86c
LhenardT <lr.ltrinidad@mmdc.mcl.edu.ph>
2025-10-08 06:16:05 +0800

    Delete petTapp-web directory

--- petTapp-web/src/components/modules/pet-owner/businesses/index.ts
+++
@@ -1,8 +0,0 @@
-export { BusinessCard } from "./BusinessCard";
-export { BusinessDetail } from "./BusinessDetail";
-export { BusinessHero } from "./BusinessHero";
-export { BusinessContact } from "./BusinessContact";
-export { BusinessLocation } from "./BusinessLocation";
-export { BusinessCredentials } from "./BusinessCredentials";
-export { BusinessHours } from "./BusinessHours";
-export { BusinessServices } from "./BusinessServices";
